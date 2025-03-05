93c2ea88849cb6844469431532535337645b84b3
chungonion <chungonion20@yahoo.com.hk>
2025-03-05 18:09:25 +0800

    BUMP

--- lib/src/widgets/router_app/widgets_vrouter.dart
+++ lib/src/widgets/router_app/widgets_vrouter.dart
@@ -116,7 +116,6 @@ class WidgetsVRouter extends VRouterApp {
     this.debugShowWidgetInspector = false,
     this.debugShowCheckedModeBanner = true,
     this.useInheritedMediaQuery = false,
-    this.inspectorSelectButtonBuilder,
     this.shortcuts,
     this.actions,
     this.restorationScopeId,
@@ -344,7 +343,7 @@ class WidgetsVRouter extends VRouterApp {
   /// This lets [MaterialApp] to use a material button to toggle the inspector
   /// select mode without requiring [WidgetInspector] to depend on the
   /// material package.
-  final InspectorSelectButtonBuilder? inspectorSelectButtonBuilder;
+  // final InspectorSelectButtonBuilder? inspectorSelectButtonBuilder;
 
   /// {@macro flutter.widgets.widgetsApp.shortcuts}
   /// {@tool snippet}
@@ -491,7 +490,6 @@ class WidgetsVRouterState extends State<WidgetsVRouter>
         debugShowWidgetInspector: widget.debugShowWidgetInspector,
         debugShowCheckedModeBanner: widget.debugShowCheckedModeBanner,
         // useInheritedMediaQuery: widget.useInheritedMediaQuery,
-        inspectorSelectButtonBuilder: widget.inspectorSelectButtonBuilder,
         shortcuts: widget.shortcuts,
         actions: widget.actions,
         restorationScopeId: widget.restorationScopeId,
